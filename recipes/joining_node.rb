8bafe7b14646c84c96e2870b9fce65fac7dec3da
Giovanni Sakti <giosakti@gmail.com>
2018-08-09 15:12:27 +0700

    [gio/tara] when joining cluster additional parameters should still be specified

--- recipes/joining_node.rb
+++ recipes/joining_node.rb
@@ -27,11 +27,15 @@ template '/etc/default/lxd_preseed.yml' do
   mode '0755'
   variables(:lxd_bind_address =>  node_ipaddress,
             :server_name => node.name,
+            :network_bridge_name => node[cookbook_name][:network_bridge_name],
+            :underlay_subnet => node[cookbook_name][:underlay_subnet],
+            :overlay_subnet => node[cookbook_name][:overlay_subnet],
             :lxd_cluster_address => node[cookbook_name][:lxd_cluster_address],
             :lxd_cluster_certificate => node[cookbook_name][:lxd_cluster_certificate],
             :lxd_cluster_password => node[cookbook_name][:lxd_cluster_password],
             :storage_pool_source => node[cookbook_name][:storage_pool_source],
             :storage_pool_name => node[cookbook_name][:storage_pool_name],
+            :storage_pool_driver => node[cookbook_name][:storage_pool_driver],
             :ssh_authorized_key => node[cookbook_name][:ssh_authorized_key])
 end
 
